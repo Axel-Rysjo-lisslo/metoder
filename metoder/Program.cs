886e21801d62e0c553ada805243c1974e650f73c
AxelRysjöLisslö <rysjolisslo.axel@elev.ga.ntig.se>
2022-03-29 14:09:33 +0200

    initial commit

---
+++ metoder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
